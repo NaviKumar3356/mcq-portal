6133790a9e8c97df01f4ac56fbb65baec372cca5
NaveenAlphonic <195666022+NaviKumar3356@users.noreply.github.com>
2026-08-09 22:19:37 +0530

    Colorful student login redesign, bigger logo

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -2,9 +2,9 @@ export const SCHOOL_NAME = 'Sant Nandlal Smriti Vidya Mandir';
 export const SCHOOL_SHORT = 'SNSVM';
 export const SCHOOL_PLACE = 'Malsisar, Rajasthan';
 
-export const CLASSES = [
-  '1st', '2nd', '3rd', '4th', '5th', '6th',
-  '7th', '8th', '9th', '10th', '11th', '12th',
+const CLASSES = [
+  'I', 'II', 'III', 'IV', 'V', 'VI',
+  'VII', 'VIII', 'IX', 'X', 'XI', 'XII',
 ];
 
 export const SUBJECTS = [
