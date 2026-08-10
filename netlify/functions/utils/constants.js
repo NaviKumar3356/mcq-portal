d9956cdac2e26821467d7bb4d9a2b564c0f4369d
NaveenAlphonic <195666022+NaviKumar3356@users.noreply.github.com>
2026-08-10 10:15:49 +0530

    Update constants.js
    "Creating Paper Updated "

--- netlify/functions/utils/constants.js
+++ netlify/functions/utils/constants.js
@@ -1,6 +1,6 @@
 const CLASSES = [
-  '1st', '2nd', '3rd', '4th', '5th', '6th',
-  '7th', '8th', '9th', '10th', '11th', '12th',
+  'I', 'II', 'III', 'IV', 'V', 'VI',
+  'VII', 'VIII', 'IX', 'X', 'XI', 'XII',
 ];
 
 const SUBJECTS = [
